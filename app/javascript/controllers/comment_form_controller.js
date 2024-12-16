790160d8cf8ea786472c031932ca9a5637881d23
Arnaud Dubois <arnaudduboislondon@gmail.com>
2024-12-16 11:36:42 +0100

    star ratign fixed

--- app/javascript/controllers/comment_form_controller.js
+++ app/javascript/controllers/comment_form_controller.js
@@ -1,17 +1,22 @@
-import { Controller } from "stimulus";
-import { useSubmit } from "stimulus-use";
+import { Controller } from "@hotwired/stimulus"
 
+// Connects to data-controller="comment-form"
 export default class extends Controller {
   static targets = ["form"];
 
   connect() {
-    useSubmit(this);  // Utilisation de stimulus-use pour simplifier le submit
+    console.log("comment form connected");
+    //useSubmit(this);  // Utilisation de stimulus-use pour simplifier le submit
   }
 
 
   submit(event) {
-    event.preventDefault();  // Empêche la soumission normale du formulaire
-    this.element.querySelector("form").requestSubmit(); // Soumet le formulaire via Turbo
-    this.element.querySelector("form").reset();  // Réinitialise le formulaire
+    //event.preventDefault();  // Empêche la soumission normale du formulaire
+    this.element.requestSubmit(); // Soumet le formulaire via Turbo
+  }
+
+  reset() {
+    console.log("reset");
+    this.element.reset();  // Réinitialise le formulaire
   }
 }
