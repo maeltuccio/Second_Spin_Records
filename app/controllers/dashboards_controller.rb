0dd4eb95bf30c112e756934add76799894665f9b
Arnaud Dubois <arnaudduboislondon@gmail.com>
2024-12-18 14:53:00 +0100

    les reco sont dans le dash

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,5 +2,9 @@ class DashboardsController < ApplicationController
   def index
     @user = current_user
     @recommendations = current_user.wishlists.map(&:disc)
+    @disc_reco = Disc.find(@recommendations.sample.id)
+    @disc = Disc.where(genre: @disc_reco.genre)
+    @discs = @disc.where.not(id: @disc_reco.id) if @disc.count > 1
+
   end
 end
